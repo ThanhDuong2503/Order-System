303309e98fa90ef12f39e1db3479de14535a4383
Thanh <Thanh.Duong2503@gmx.de>
2020-05-22 10:08:36 +0200

    initialcommit

---
+++ src/main/java/de/neuefische/OrderSystem/AppMain.java
@@ -0,0 +1,9 @@
+package de.neuefische.OrderSystem;
+
+public class AppMain {
+    public static void main(String[] args){
+
+
+
+    }
+}
---
+++ src/main/java/de/neuefische/OrderSystem/model/ProductInterface.java
@@ -0,0 +1,7 @@
+package de.neuefische.OrderSystem.model;
+
+public class ProductInterface {
+    int id;
+    String name;
+
+}
